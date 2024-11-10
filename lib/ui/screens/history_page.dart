bd04b2ce32a09f0886b1ff0bbbd9df6d189463c2
htrang <hongtrangqn834@gmail.com>
2024-11-10 23:50:40 +0700

    10.11

--- lib/ui/screens/history_page.dart
+++ lib/ui/screens/history_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:plant/constants.dart';
+import 'package:plant/models/plants.dart';
+import 'package:plant/ui/screens/widgets/plant_widget.dart';
 
 class HistoryPage extends StatefulWidget {
-  const HistoryPage({super.key});
+  final List<Plant> favoritedPlants;
+  const HistoryPage({Key? key, required this.favoritedPlants})
+      : super(key: key);
 
   @override
   State<HistoryPage> createState() => _HistoryPageState();
@@ -10,14 +15,44 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: Text('History Page'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-        ),
-        body: const Center(
-          child: Text('History Page'),
-        ));
+      body: widget.favoritedPlants.isEmpty
+          ? Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset('assets/images/favorited.png'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Your searched Plants',
+                    style: TextStyle(
+                      color: Constants.primaryColor,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 30),
+              height: size.height * .5,
+              child: ListView.builder(
+                  itemCount: widget.favoritedPlants.length,
+                  scrollDirection: Axis.vertical,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    return PlantWidget(
+                        index: index, plantList: widget.favoritedPlants);
+                  }),
+            ),
+    );
   }
 }
