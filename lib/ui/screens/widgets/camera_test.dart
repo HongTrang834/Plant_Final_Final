42f09524932f7ce11684579a765f3e9cf1bd4021
htrang <hongtrangqn834@gmail.com>
2024-11-16 23:55:56 +0700

    16.11

---
+++ lib/ui/screens/widgets/camera_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraTest extends StatefulWidget {
+  const CameraTest({super.key});
+
+  @override
+  State<CameraTest> createState() => _CameraTestState();
+}
+
+class _CameraTestState extends State<CameraTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
